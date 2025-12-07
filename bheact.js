09d0b04d5d4d6d7eb3d426e6230e9f49850e7c17
Bhavesh Kukreja <bhaveshkukreja29@gmail.com>
2025-12-07 08:52:19 +0530

    Now replacing the function with it's output cuz we jus want pure js

--- bheact.js
+++ bheact.js
@@ -1,11 +1,32 @@
-// JSX is transformed into JS by build tools like Babel
-// so JSX -> createElement call passing tag name, props and children as args
-// const element = <h1 title="bar">Hello, World!</h1>
-const element = React.createElement(
-    "h1",
-    { title: "bar"},
-    "Hello, World!"
-)
+// createElement creates the object you want throught the args you pass
+// besides some validation, it doesn't do much, so we safely can replace
+// the function call with it's output
+const element = {
+    type: "h1",
+    props: {
+        title: "bar",
+        children: "Hello, World!",
+    },
+}
+// and this is what an element is. an object with two properties 
+// there is more like key, ref, self, source, owner
+// source code: 
+/*
+const ReactElement = function(type, key, ref, self, source, owner, props) {
+  const element = {
+    // This tag allows us to uniquely identify this as a React Element
+    $$typeof: REACT_ELEMENT_TYPE,
+
+    // Built-in properties that belong on the element
+    type: type,
+    key: key,
+    ref: ref,
+    props: props,
+
+    // Record the component responsible for creating this element.
+    _owner: owner,
+  };
+*/
 
 const container = document.getElementById("root")
 ReactDOM.render(element, container)
