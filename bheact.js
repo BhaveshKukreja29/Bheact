867396096dde92aef17e05fae23ede6923ce66d2
Bhavesh Kukreja <bhaveshkukreja29@gmail.com>
2025-12-07 08:07:27 +0530

    Converting JSX to a valid JS function call

--- bheact.js
+++ bheact.js
@@ -1,6 +1,11 @@
-// this is some basic jsx, and react render, none of this works rn
-// next we'll replace element to vanilla js
-// cuz under the hood jsx get converted into js somehow
-const element = <h1 title="bar">Hello, World!</h1>
+// JSX is transformed into JS by build tools like Babel
+// so JSX -> createElement call passing tag name, props and children as args
+// const element = <h1 title="bar">Hello, World!</h1>
+const element = React.createElement(
+    "h1",
+    { title: "bar"},
+    "Hello, World!"
+)
+
 const container = document.getElementById("root")
 ReactDOM.render(element, container)
