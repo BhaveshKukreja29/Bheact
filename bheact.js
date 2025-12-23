62e76464006dad33f87f1986d800ca98896e7cbf
Bhavesh Kukreja <bhaveshkukreja29@gmail.com>
2025-12-24 00:40:25 +0530

    Implement createElement and createTextElement

--- bheact.js
+++ bheact.js
@@ -1,9 +1,37 @@
+function createElement(type, props, ...children) {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map(child =>
+        typeof child === "object"
+          ? child
+          : createTextElement(child)
+      ),
+    },
+  }
+}
+
+// children may contain primitive values like strings or numbers
+// for those we simply create a text node. even though react doesn't do this
+// we want to keep this simple
+
+function createTextElement(text) {
+  return {
+    type: "TEXT_ELEMENT",
+    props: {
+      nodeValue: text,
+      children: [],
+    },
+  }
+}
+
 // convert JSX to JS
 const element = React.createElement(
   "div",
   { id: "foo" },
   React.createElement("a", null, "bar"),
   React.createElement("b"),
-) // now the only thing that our function needs to do is create this element
+)
 const container = document.getElementById("root")
 ReactDOM.render(element, container)
